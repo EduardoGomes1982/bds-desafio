f12b9338ac12bde1ebbc78afb331c45aa368270f
EduardoGomes1982 <edu1982tal@gmail.com>
2023-12-25 18:55:53 -0300

    NavBar ended

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,11 @@
 import NavBar from 'components/NavBar';
+import { BrowserRouter } from 'react-router-dom';
 
 const Routes = () => (
-    <>
+    <BrowserRouter>
         <NavBar />
         <h1>Algo aqui</h1>
-    </>
+    </BrowserRouter>
 );
 
 export default Routes;
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,20 +1,29 @@
 import './styles.css';
 import '@popperjs/core';
 import 'bootstrap/js/dist/collapse';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className='bg-primary navbar navbar-expand-md navbar-dark main-nav'>
-            <div><h4>Carros Top</h4></div>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ct-button" aria-controls="ct-button"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div id='ct-button' className=' collapse'>
-                <ul className='navbar-nav offset-md-2 main-menu'>
-                    <li>Home</li>
-                    <li>Catálogo</li>
-                </ul>
+        <nav className='bg-primary navbar navbar-expand-md navbar-dark main-nav nav-logo-text'>
+            <div className='container-fluid container-lg'>
+                <Link to='/'>
+                    <h4>Carros Top</h4>
+                </Link>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#ct-button' aria-controls='ct-button'
+                    aria-expanded='false' aria-label='Toggle navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div id='ct-button' className='collapse navbar-collapse'>
+                    <ul className='navbar-nav offset-sm-8 main-menu'>
+                        <NavLink to='/' activeClassName='active' exact>
+                            <li>Home</li>
+                        </NavLink>
+                        <NavLink to='/products' activeClassName='active'>
+                            <li>Catálogo</li>
+                        </NavLink>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
